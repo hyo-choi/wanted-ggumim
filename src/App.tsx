15311c8c30ec3311d0e05cc47183901757526914
hyo-choi <hyochoi31@gmail.com>
2022-01-31 19:15:10 +0900

    :recycle: App 임시 렌더링 로직 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable no-use-before-define */
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import type { ResponseType } from '~types/index';
 import { fetchApi } from '~api/index';
 
 const App = () => {
-  const [state, setState] = useState({});
+  const [state, setState] = useState<ResponseType>();
 
   useEffect(() => {
     const getData = async () => {
@@ -11,9 +14,30 @@ const App = () => {
     getData();
   }, []);
 
+  if (!state) {
+    return <Layout>loading...</Layout>;
+  }
+
   return (
-    <div>{JSON.stringify(state)}</div>
+    <Layout>
+      <h1>{state.id}</h1>
+      <img src={state.imageUrl} alt="방 이미지" />
+      {state.productList.map(({ productId, productName, imageUrl }) => (
+        <React.Fragment key={productId}>
+          <div>{productName}</div>
+          <img src={imageUrl} alt={`${productName} 이미지`} />
+        </React.Fragment>
+      ))}
+    </Layout>
   );
 };
+
+const Layout = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1em 2em;
+`;
 
 export default App;
