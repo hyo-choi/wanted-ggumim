dfc25979b1d78cd439b1ba19d834a5815b336e9d
hyo-choi <hyochoi31@gmail.com>
2022-01-31 22:49:04 +0900

    :bug: room image 초기 너비 지정

--- src/utils/calculation.ts
+++ src/utils/calculation.ts
@@ -1,7 +1,7 @@
-/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable import/prefer-default-export */
-/* eslint-disable no-confusing-arrow */
 import { IMAGE_MAX_SIZE } from '~constants/index';
 
-export const calcRoomInfoWidth = (parentWidth: number): number =>
-  IMAGE_MAX_SIZE > parentWidth ? parentWidth : IMAGE_MAX_SIZE;
+export const calcRoomInfoWidth = (parentWidth: number): number => {
+  if (parentWidth === 0) return IMAGE_MAX_SIZE;
+  return IMAGE_MAX_SIZE > parentWidth ? parentWidth : IMAGE_MAX_SIZE;
+};
