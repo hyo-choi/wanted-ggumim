51d55714441593e88ec55d53bf7e381d61954abe
hyo-choi <hyochoi31@gmail.com>
2022-01-31 17:36:12 +0900

    :sparkles: api fetch 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchApi } from '@api/index';
 
-const App = () => (
-  <div />
-);
+const App = () => {
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      setState(await fetchApi());
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>{JSON.stringify(state)}</div>
+  );
+};
 
 export default App;
---
+++ src/api/fetchApi.ts
@@ -0,0 +1,9 @@
+import { API_ORIGIN, API_PATH } from '@constants/index';
+
+const fetchApi = async () => {
+  const result = await fetch(`${API_ORIGIN}${API_PATH}`);
+  const data = await result.json();
+  return data;
+};
+
+export default fetchApi;
